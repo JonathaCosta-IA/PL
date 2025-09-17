61e1c1fd43e421a8635bd4a60b0bb00b0675b641
Jonatha R Costa <92397588+JonathaCosta-IA@users.noreply.github.com>
2025-09-17 11:47:41 -0300

    Update

--- B_PL_Codes/13-Comando_while/while03.c
+++ B_PL_Codes/13-Comando_while/while03.c
@@ -2,13 +2,13 @@
 Lógica de programação
 Prof. Jonatha Costa
 Exercício resolvido:
-Escreva um programa que solicite um número ao usuário, e mostre sua tabuada completa (de 1 até 10).
+Escreva um programa que solicite um número ao usuário, e mostre sua tabuada de multiplicação completa (de 1 até 10).
 */
 #include <stdio.h>
 int main() {
     int numero, i = 1;
     // Solicitar ao usuário que insira um número
-    printf("Digite um número para ver a sua tabuada: ");
+    printf("Digite um número para ver a sua tabuada de multiplicação: ");
     scanf("%d", &numero);
     // Laço while para gerar a tabuada de 1 a 10
     while (i <= 10) {
