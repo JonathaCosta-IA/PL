33c2d6af0a4625532b27bea39556122c0b2f3dbc
Jonatha R Costa <92397588+JonathaCosta-IA@users.noreply.github.com>
2026-04-07 14:40:57 -0300

    Update

--- B_PL_Codes/10-Comando_if/if01.c
+++ B_PL_Codes/10-Comando_if/if01.c
@@ -1,27 +1,33 @@
 /*
 Lógica de programação
 Prof. Jonatha Costa
-Exercício resolvido:
+Exercício resolvido:8
 Escreva uma programa que leia uma nota e verifique se aprovado ou não,
 considerando 7,0 como critério.
 Versão 1: Código básico para passo-a-passo, sem blocos de código.
 
 
 *****************************************************
-Exercício : Reescreva o código utilizando outra lógica, estratégia ou comandos. 
+Exercício : Reescreva o código utilizando outra lógica, estratégia ou comandos.
 *****************************************************
 */
 
 #include <stdio.h>
-int main(){
-float nota;
-// Ler dados
-printf("Informe uma nota:\n");
-scanf("%f",&nota);
-// Testar
-if (nota>=7.0)
-// Exibir resultados
-{printf("Candidato aprovado!\n");}
-else {printf("Candidado não aprovado!\n");}
-return 0;
+int main()
+{
+    float nota;
+    // Ler dados
+    printf("Informe uma nota:\n");
+    scanf("%f", &nota);
+    // Testar
+    if (nota >= 7.0)
+    // Exibir resultados
+    {
+        printf("Candidato aprovado!\n");
+    }
+    else
+    {
+        printf("Candidado não aprovado!\n");
+    }
+    return 0;
 }
