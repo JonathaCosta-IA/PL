c83453d87bd86e72e708db27b312c018dabaf17c
Jonatha R Costa <92397588+JonathaCosta-IA@users.noreply.github.com>
2026-04-07 15:58:17 -0300

    Update

--- B_PL_Codes/10-Comando_if/if02.c
+++ B_PL_Codes/10-Comando_if/if02.c
@@ -7,17 +7,26 @@ considerando como critério de aprovação média 7,0;
 */
 
 #include <stdio.h>
-int main(){
-float nota1,nota2,media;
-// Ler dados
-printf("Informe uma nota 1:\n"); scanf("%f",&nota1);
-printf("Informe uma nota 2:\n"); scanf("%f",&nota2);
-// Calcular
-media=(nota1+nota2)/2;
-// Testar
-if (media>=7.0)
-// Exibir resultados
-{printf("Candidato aprovado!\n");}
-else {printf("Candidado não aprovado!\n");}
-return 0;
+int main()
+{
+    float nota1, nota2, media;
+    // Ler dados
+    printf("Informe uma nota 1:\n");
+    scanf("%f", &nota1);
+    printf("Informe uma nota 2:\n");
+    scanf("%f", &nota2);
+    // Calcular
+    media = (nota1 + nota2) / 2;
+    // Testar
+    if (media >= 7.0)
+    // Exibir resultados
+    {
+        printf("Candidato aprovado!\n");
+    }
+    else
+    {
+        printf("Candidado não aprovado!\n");
+    }
+
+    return 0;
 }
