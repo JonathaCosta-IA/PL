3f5da60f2f04138f1122d6801aad53a1745b7b75
Jonatha R Costa <92397588+JonathaCosta-IA@users.noreply.github.com>
2026-04-09 20:52:45 -0300

    Update

--- B_PL_Codes/22-ExecSala/matr01.c
+++ B_PL_Codes/22-ExecSala/matr01.c
@@ -14,8 +14,10 @@ g) Exibir somente os ímpares de cada matriz
 
 #include <stdio.h>
 void Exibir_Traco_Matriz(int A[3][3]);
-void ChamarTracos(int A[3][3],int B[3][3],int C[3][3]);
-void Exibir_Somatorio_Xij_Matrizes(int A[3][3],int B[3][3],int C[3][3]);
+void ChamarTracos(int A[3][3], int B[3][3], int C[3][3]);
+void Exibir_Somatorio_Xij_Matrizes(int A[3][3], int B[3][3], int C[3][3]);
+
+int CalculeDetMatriz(int A[3][3]);
 
 int main()
 {
@@ -24,14 +26,27 @@ int main()
     int B[3][3] = {{0, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int C[3][3] = {{10, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     // Criar uma rotina para calcular o traço da matriz e exibir
-    // ChamarTracos(A,B,C);
+    ChamarTracos(A, B, C);
 
     // Criar uma rotina para calcular a soma dos produtos aij * bij * cij
-    Exibir_Somatorio_Xij_Matrizes(A,B,C);
+    Exibir_Somatorio_Xij_Matrizes(A, B, C);
+
     // Criar uma rotina para calcular o determinante da matriz e exibir
 
+    int Det[3] = {CalculeDetMatriz(A), CalculeDetMatriz(B), CalculeDetMatriz(C)};
+    for (int i = 0; i < 3; i++)
+    {
+        printf("\nO determinante da matriz %d é %d.", i + 1, Det[i]);
+    }
+    printf("\n");
     // Criar uma rotina para testar se as matrizes são inversíveis
-
+    for (int i = 0; i < 3; i++)
+    {
+        if (Det[i] == 0)
+            printf("\nA matriz %d não é inversível, pois seu determinante é %d.", i + 1, Det[i]);
+        else
+            printf("\nA matriz %d é inversível, pois seu determinante é diferente de zero. Det = %d.", i + 1, Det[i]);
+    }
     // Criar uma rotina para calcular a média de cada matriz
 
     // Criar uma rotina para verificar se o somatório das matrizes é maior ou igual a media
@@ -50,31 +65,37 @@ void Exibir_Traco_Matriz(int A[3][3])
     printf("\nO traço da matriz é %d.", traco);
 }
 
-void ChamarTracos(int A[3][3],int B[3][3],int C[3][3])
+void ChamarTracos(int A[3][3], int B[3][3], int C[3][3])
 {
     Exibir_Traco_Matriz(A);
     Exibir_Traco_Matriz(B);
     Exibir_Traco_Matriz(C);
 }
 
-void Exibir_Somatorio_Xij_Matrizes(int A[3][3],int B[3][3],int C[3][3])
+void Exibir_Somatorio_Xij_Matrizes(int A[3][3], int B[3][3], int C[3][3])
 {
-    int D[3][3];
-    // Carregando a matriz D
-    for (int i=0; i<3;i++)
-    {     for (int j=0; j<3;j++)
+
+    int soma = 0; // Variável auxiliar
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
         {
-            D[i][j] =  A[i][j]*B[i][j]*C[i][j];         
+            soma += A[i][j] * B[i][j] * C[i][j];
         }
     }
-    // Somando os valore da matriz D
-    int soma;
-    for (int i=0; i<3;i++)
-    {     for (int j=0; j<3;j++)
-        {
-           soma+= D[i][j];         
-        }
-    }
-    printf("O somatório Aib*Bij*Cij é %d.",soma);
+    printf("\n\nO somatório [A(ib) x B(ij) x C(ij)] é %d.\n", soma);
 }
 
+int CalculeDetMatriz(int A[3][3])
+{
+    int det;
+    det = (A[0][0] * A[1][1] * A[2][2] +
+           A[0][1] * A[1][2] * A[2][0] +
+           A[0][2] * A[1][0] * A[2][1]) -
+
+          (A[0][2] * A[1][1] * A[2][0] +
+           A[0][0] * A[1][2] * A[2][1] +
+           A[0][1] * A[1][0] * A[2][2]);
+
+    return det;
+}
